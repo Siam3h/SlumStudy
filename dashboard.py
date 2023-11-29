22716add79f0ce10de7902bc6831c45ecff25777
siam3h <philosiama@gmail.com>
2023-11-29 09:57:04 +0300

    Moved all code into if df= else code block

--- dashboard.py
+++ dashboard.py
@@ -189,7 +189,164 @@ if fl is not None:
                         df3["POSITIONALITY OF ARTICLE"].isin(positionality)  & 
                         df3["SECTION OF THE ARTICLE ANALYSED"].isin(sectionAnalysed) & 
                         df3["YEAR OF PUBLICATION"].isin(yearofPub) & 
-                        df3["CATEGORISATION OF PUBLICATION( descriptive, diagnostic(identify problems), Prescriptive, theoretical)"].isin(categorization)]
+                            df3["CATEGORISATION OF PUBLICATION( descriptive, diagnostic(identify problems), Prescriptive, theoretical)"].isin(categorization)]
+        
+    page = st.sidebar.selectbox("Select a page", ["Home","Word Cloud"])
+    if page == "Home":
+        
+        st.markdown("<p style='text-align: center; padding: 1rem; color: Green;'>This is the home page. Use the sidebar to navigate between pages.</p>", unsafe_allow_html=True)
+
+        ## Check if the filtered DataFrame is not empty before proceeding with visualizations
+        if not filtered_df.empty:
+            # Display the filtered data
+            st.markdown("<h3 style='text-align: center;'>Filtered Data</h3>", unsafe_allow_html=True)
+            st.write(filtered_df)
+
+            st.markdown("<h2 style='text-align: center; padding-top: 1rem; padding-bottom: 2rem;'>Column Graphs</h2>", unsafe_allow_html=True)
+
+            col1, col2 = st.columns(2)
+
+            # Visualization 1 - DOCUMENT TYPE (Bar Graph)
+            fig1 = px.bar(filtered_df['DOCUMENT TYPE (book, Journal article, Report, Statutes, Newspaper)'].value_counts(), x=filtered_df['DOCUMENT TYPE (book, Journal article, Report, Statutes, Newspaper)'].value_counts().index, y=filtered_df['DOCUMENT TYPE (book, Journal article, Report, Statutes, Newspaper)'].value_counts().values, labels={'x': 'DOCUMENT TYPE (book, Journal article, Report, Statutes, Newspaper)', 'y': 'Count'}, title='Document Type Distribution')
+            fig1.update_layout(height=500, width=600)
+            fig1.update_layout(margin=dict(l=20, r=20))
+            fig1.update_layout(title=dict(x=0.5, y=1, xanchor='center', yanchor='top'))
+            col1.plotly_chart(fig1)
+
+            # Visualization 2 - APPLICABLE NOMENCLATURE (Bar Graph)
+            fig2 = px.bar(filtered_df['APPLICABLE NOMENCLATURE(KEY NAME USED)'].value_counts(), x=filtered_df['APPLICABLE NOMENCLATURE(KEY NAME USED)'].value_counts().index, y=filtered_df['APPLICABLE NOMENCLATURE(KEY NAME USED)'].value_counts().values, labels={'x': 'APPLICABLE NOMENCLATURE(KEY NAME USED)', 'y': 'Count'}, title='Nomenclature Distribution')
+            fig2.update_layout(height=500, width=550)
+            fig2.update_layout(margin=dict(l=155, r=10))
+            fig2.update_layout(title=dict(x=0.5, y=1, xanchor='center', yanchor='top'))
+            col2.plotly_chart(fig2)
+
+            # Visualization 3 - YEAR OF PUBLICATION (Line Chart)
+            fig3 = px.line(df['YEAR OF PUBLICATION'].value_counts().sort_index(), labels={'x': 'Year of Publication', 'y': 'Count'},
+                        title='Year of Publication Distribution', markers=True)
+            
+            fig3.update_layout(height=600, width=550 )
+            fig3.update_layout(margin=dict(l=20, r=30))
+            fig3.update_layout(title=dict(x=0.5, y=1, xanchor='center', yanchor='top'))
+
+            # Remove white backgrounds
+            fig3.update_layout({
+                'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+                'paper_bgcolor': 'rgba(0, 0, 0, 0)',
+            })
+
+            # Visualization 6 - SECTION OF THE ARTICLE ANALYSED (Bar Graph)
+            fig6 = px.bar(filtered_df['SECTION OF THE ARTICLE ANALYSED'].value_counts(), x=filtered_df['SECTION OF THE ARTICLE ANALYSED'].value_counts().index, y=filtered_df['SECTION OF THE ARTICLE ANALYSED'].value_counts().values, labels={'x': 'Section', 'y': 'Count'}, title='Section Distribution')
+            fig6.update_layout(height=500, width=550)
+            fig6.update_layout(margin=dict(l=155, r=10))
+            fig6.update_layout(title=dict(x=0.5, y=1, xanchor='center', yanchor='top'))
+            col2.plotly_chart(fig6)
+
+            # Visualization 7 - POSITIONALITY OF ARTICLE (Bar Graph)
+            fig7 = px.bar(filtered_df['POSITIONALITY OF ARTICLE'].value_counts(), x=filtered_df['POSITIONALITY OF ARTICLE'].value_counts().index, y=filtered_df['POSITIONALITY OF ARTICLE'].value_counts().values, labels={'x': 'Positionality', 'y': 'Count'}, title='Positionality Distribution')
+            fig7.update_layout(height=500, width=600)
+            fig7.update_layout(margin=dict(l=20, r=20))
+            fig7.update_layout(title=dict(x=0.5, y=1, xanchor='center', yanchor='top'))
+            col1.plotly_chart(fig7)
+
+            # Visualization 4 - CONTINENT OF STUDY (Pie Chart)
+            fig4 = px.pie(filtered_df, names='CONTINENT OF STUDY', title='Continent of Study Distribution')
+            fig4.update_layout(height=500, width=500)
+            fig4.update_layout(margin=dict(l=20, r=20))
+            fig4.update_layout(title=dict(x=0.5, y=1, xanchor='center', yanchor='top'))
+            col2.plotly_chart(fig4)
+
+            # Visualization 5 - NATIONALITY/ORIGIN/EXTRACTION OF AUTHOR (Pie Chart)
+            fig5 = px.pie(filtered_df, names='NATIONALITY/ORIGIN/EXTRACTION OF AUTHOR', title='Nationality Distribution')
+            fig5.update_layout(height=500, width=500)
+            fig5.update_layout(margin=dict(l=20, r=20))
+            fig5.update_layout(title=dict(x=0.5, y=1, xanchor='center', yanchor='top'))
+            col1.plotly_chart(fig5)
+
+            # Visualization 8 - USE OF STATISTICS (Pie Chart)
+            fig8 = px.pie(filtered_df, names='USE OF STATISTICS', title='Use of Statistics Distribution')
+            fig8.update_layout(height=500, width=500)
+            fig8.update_layout(margin=dict(l=20, r=20))
+            fig8.update_layout(title=dict(x=0.5, y=1, xanchor='center', yanchor='top'))
+            col2.plotly_chart(fig8)
+
+            # Visualization 9 - CITE 'UN-habitat 2003 STUDY ON SLUMS' (Pie Chart)
+            fig9 = px.pie(filtered_df, names='CITE \'UN-habitat 2003 STUDY ON SLUMS\'', title='Cite UN-habitat 2003 Study on Slums Distribution')
+            fig9.update_layout(height=500, width=500)
+            fig9.update_layout(margin=dict(l=20, r=20))
+            fig9.update_layout(title=dict(x=0.5, y=1, xanchor='center', yanchor='top'))
+            col1.plotly_chart(fig9)
+
+            # Visualization 10 - CATEGORISATION OF PUBLICATION (Pie Chart)
+            fig10 = px.pie(filtered_df, names='CATEGORISATION OF PUBLICATION( descriptive, diagnostic(identify problems), Prescriptive, theoretical)', title='Categorization of Publication Distribution')
+            fig10.update_layout(height=500, width=500)
+            fig10.update_layout(margin=dict(l=35, r=10))
+            fig10.update_layout(title=dict(x=0.5, y=1, xanchor='center', yanchor='top'))
+            col2.plotly_chart(fig10)
+
+            #Visualization for Year of Publication
+            col1.plotly_chart(fig3)
+
+        else:
+            st.warning("No data found for the selected filters.")
+        
+
+    elif page == "Word Cloud":
+        st.markdown("<h2 style='text-align: center; padding-top: 1rem;'>Word Clouds</h2>", unsafe_allow_html=True)
+        # Word Cloud
+        st.markdown("""
+        <div style="display: flex; justify-content: center; padding-top: 1rem;">
+            <h3>Adjectival Phrases Word Cloud</h3>
+        </div>
+    """, unsafe_allow_html=True)
+        text_data = 'DESCRIPTIVE TERMS/ADJECTIVES/ADJECTIVAL PHRASES (e.g. filthy, dirty, undesirable)'  
+        wordcloud = WordCloud(width=600, height=400, background_color='Black', mode='RGBA').generate(' '.join(filtered_df[text_data].dropna()))
+        plt.figure(figsize=(10, 5))
+        plt.imshow(wordcloud, interpolation='bilinear')
+        plt.axis('off')
+        st.set_option('deprecation.showPyplotGlobalUse', False)
+        st.pyplot()
+
+
+        col3, col4 = st.columns((2))
+
+        col3.subheader('Descriptive studies Word Cloud')
+        text_data = 'Descriptive studies( nature of slums(conceptualization), health concerns, housing, water, sanitation, nutrition,land, approaches to slum Upgrading)'  
+        wordcloud = WordCloud(width=400, height=400, background_color='#3498db', mode='RGBA').generate(' '.join(filtered_df[text_data].dropna()))
+        plt.figure(figsize=(10, 5))
+        plt.imshow(wordcloud, interpolation='bilinear')
+        plt.axis('off')
+        st.set_option('deprecation.showPyplotGlobalUse', False)
+        col3.pyplot()
+
+        col3.subheader('Prescriptive Studies Word Cloud')
+        text_data = 'Prescriptive[inclusivity/tackling marginalisation, slum upgrading/improvement, security improvement,'  
+        wordcloud = WordCloud(width=400, height=400, background_color='#2ecc71', mode='RGBA').generate(' '.join(filtered_df[text_data].dropna()))
+        plt.figure(figsize=(10, 5))
+        plt.imshow(wordcloud, interpolation='bilinear')
+        plt.axis('off')
+        st.set_option('deprecation.showPyplotGlobalUse', False)
+        col3.pyplot()
+
+        col4.subheader('Diagnostic Studies Word Cloud')
+        text_data = 'Diagnostic (strategies, policy gaps/challenges, tenure concerns, public health concerns, environmental concerns, marginalisation(inclusion, exclusion)]'  
+        wordcloud = WordCloud(width=400, height=400, background_color='#e74c3c', mode='RGBA').generate(' '.join(filtered_df[text_data].dropna()))
+        plt.figure(figsize=(10, 5))
+        plt.imshow(wordcloud, interpolation='bilinear')
+        plt.axis('off')
+        st.set_option('deprecation.showPyplotGlobalUse', False)
+        col4.pyplot()
+
+        col4.subheader('Theoretical Studies Word Cloud')
+        text_data = 'Theoretical( theories, liberal/neo-liberal theories, colonial/post colonial theories, political economy)'  
+        wordcloud = WordCloud(width=400, height=400, background_color='#f39c12', mode='RGBA').generate(' '.join(filtered_df[text_data].dropna()))
+        plt.figure(figsize=(10, 5))
+        plt.imshow(wordcloud, interpolation='bilinear')
+        plt.axis('off')
+        st.set_option('deprecation.showPyplotGlobalUse', False)
+        col4.pyplot()
+
+    else:
+        st.warning('No Data Available')
             
 else:
     st.warning("Please upload a CSV file.")
@@ -210,159 +367,3 @@ else:
 page = st.sidebar.selectbox("Select a page", ["Home","Word Cloud"])
 
 #Analysis Logic
-
-if page == "Home":
-    
-    st.markdown("<p style='text-align: center; padding: 1rem; color: Green;'>This is the home page. Use the sidebar to navigate between pages.</p>", unsafe_allow_html=True)
-
-    ## Check if the filtered DataFrame is not empty before proceeding with visualizations
-    if not filtered_df.empty:
-        # Display the filtered data
-        st.markdown("<h3 style='text-align: center;'>Filtered Data</h3>", unsafe_allow_html=True)
-        st.write(filtered_df)
-
-        st.markdown("<h2 style='text-align: center; padding-top: 1rem; padding-bottom: 2rem;'>Column Graphs</h2>", unsafe_allow_html=True)
-
-        col1, col2 = st.columns(2)
-
-        # Visualization 1 - DOCUMENT TYPE (Bar Graph)
-        fig1 = px.bar(filtered_df['DOCUMENT TYPE (book, Journal article, Report, Statutes, Newspaper)'].value_counts(), x=filtered_df['DOCUMENT TYPE (book, Journal article, Report, Statutes, Newspaper)'].value_counts().index, y=filtered_df['DOCUMENT TYPE (book, Journal article, Report, Statutes, Newspaper)'].value_counts().values, labels={'x': 'DOCUMENT TYPE (book, Journal article, Report, Statutes, Newspaper)', 'y': 'Count'}, title='Document Type Distribution')
-        fig1.update_layout(height=500, width=600)
-        fig1.update_layout(margin=dict(l=20, r=20))
-        fig1.update_layout(title=dict(x=0.5, y=1, xanchor='center', yanchor='top'))
-        col1.plotly_chart(fig1)
-
-        # Visualization 2 - APPLICABLE NOMENCLATURE (Bar Graph)
-        fig2 = px.bar(filtered_df['APPLICABLE NOMENCLATURE(KEY NAME USED)'].value_counts(), x=filtered_df['APPLICABLE NOMENCLATURE(KEY NAME USED)'].value_counts().index, y=filtered_df['APPLICABLE NOMENCLATURE(KEY NAME USED)'].value_counts().values, labels={'x': 'APPLICABLE NOMENCLATURE(KEY NAME USED)', 'y': 'Count'}, title='Nomenclature Distribution')
-        fig2.update_layout(height=500, width=550)
-        fig2.update_layout(margin=dict(l=155, r=10))
-        fig2.update_layout(title=dict(x=0.5, y=1, xanchor='center', yanchor='top'))
-        col2.plotly_chart(fig2)
-
-        # Visualization 3 - YEAR OF PUBLICATION (Line Chart)
-        fig3 = px.line(df['YEAR OF PUBLICATION'].value_counts().sort_index(), labels={'x': 'Year of Publication', 'y': 'Count'},
-                    title='Year of Publication Distribution', markers=True)
-        
-        fig3.update_layout(height=600, width=550 )
-        fig3.update_layout(margin=dict(l=20, r=30))
-        fig3.update_layout(title=dict(x=0.5, y=1, xanchor='center', yanchor='top'))
-
-        # Remove white backgrounds
-        fig3.update_layout({
-            'plot_bgcolor': 'rgba(0, 0, 0, 0)',
-            'paper_bgcolor': 'rgba(0, 0, 0, 0)',
-        })
-
-        # Visualization 6 - SECTION OF THE ARTICLE ANALYSED (Bar Graph)
-        fig6 = px.bar(filtered_df['SECTION OF THE ARTICLE ANALYSED'].value_counts(), x=filtered_df['SECTION OF THE ARTICLE ANALYSED'].value_counts().index, y=filtered_df['SECTION OF THE ARTICLE ANALYSED'].value_counts().values, labels={'x': 'Section', 'y': 'Count'}, title='Section Distribution')
-        fig6.update_layout(height=500, width=550)
-        fig6.update_layout(margin=dict(l=155, r=10))
-        fig6.update_layout(title=dict(x=0.5, y=1, xanchor='center', yanchor='top'))
-        col2.plotly_chart(fig6)
-
-        # Visualization 7 - POSITIONALITY OF ARTICLE (Bar Graph)
-        fig7 = px.bar(filtered_df['POSITIONALITY OF ARTICLE'].value_counts(), x=filtered_df['POSITIONALITY OF ARTICLE'].value_counts().index, y=filtered_df['POSITIONALITY OF ARTICLE'].value_counts().values, labels={'x': 'Positionality', 'y': 'Count'}, title='Positionality Distribution')
-        fig7.update_layout(height=500, width=600)
-        fig7.update_layout(margin=dict(l=20, r=20))
-        fig7.update_layout(title=dict(x=0.5, y=1, xanchor='center', yanchor='top'))
-        col1.plotly_chart(fig7)
-
-        # Visualization 4 - CONTINENT OF STUDY (Pie Chart)
-        fig4 = px.pie(filtered_df, names='CONTINENT OF STUDY', title='Continent of Study Distribution')
-        fig4.update_layout(height=500, width=500)
-        fig4.update_layout(margin=dict(l=20, r=20))
-        fig4.update_layout(title=dict(x=0.5, y=1, xanchor='center', yanchor='top'))
-        col2.plotly_chart(fig4)
-
-        # Visualization 5 - NATIONALITY/ORIGIN/EXTRACTION OF AUTHOR (Pie Chart)
-        fig5 = px.pie(filtered_df, names='NATIONALITY/ORIGIN/EXTRACTION OF AUTHOR', title='Nationality Distribution')
-        fig5.update_layout(height=500, width=500)
-        fig5.update_layout(margin=dict(l=20, r=20))
-        fig5.update_layout(title=dict(x=0.5, y=1, xanchor='center', yanchor='top'))
-        col1.plotly_chart(fig5)
-
-        # Visualization 8 - USE OF STATISTICS (Pie Chart)
-        fig8 = px.pie(filtered_df, names='USE OF STATISTICS', title='Use of Statistics Distribution')
-        fig8.update_layout(height=500, width=500)
-        fig8.update_layout(margin=dict(l=20, r=20))
-        fig8.update_layout(title=dict(x=0.5, y=1, xanchor='center', yanchor='top'))
-        col2.plotly_chart(fig8)
-
-        # Visualization 9 - CITE 'UN-habitat 2003 STUDY ON SLUMS' (Pie Chart)
-        fig9 = px.pie(filtered_df, names='CITE \'UN-habitat 2003 STUDY ON SLUMS\'', title='Cite UN-habitat 2003 Study on Slums Distribution')
-        fig9.update_layout(height=500, width=500)
-        fig9.update_layout(margin=dict(l=20, r=20))
-        fig9.update_layout(title=dict(x=0.5, y=1, xanchor='center', yanchor='top'))
-        col1.plotly_chart(fig9)
-
-        # Visualization 10 - CATEGORISATION OF PUBLICATION (Pie Chart)
-        fig10 = px.pie(filtered_df, names='CATEGORISATION OF PUBLICATION( descriptive, diagnostic(identify problems), Prescriptive, theoretical)', title='Categorization of Publication Distribution')
-        fig10.update_layout(height=500, width=500)
-        fig10.update_layout(margin=dict(l=35, r=10))
-        fig10.update_layout(title=dict(x=0.5, y=1, xanchor='center', yanchor='top'))
-        col2.plotly_chart(fig10)
-
-        #Visualization for Year of Publication
-        col1.plotly_chart(fig3)
-
-    else:
-        st.warning("No data found for the selected filters.")
-    
-
-elif page == "Word Cloud":
-    st.markdown("<h2 style='text-align: center; padding-top: 1rem;'>Word Clouds</h2>", unsafe_allow_html=True)
-    # Word Cloud
-    st.markdown("""
-    <div style="display: flex; justify-content: center; padding-top: 1rem;">
-        <h3>Adjectival Phrases Word Cloud</h3>
-    </div>
-""", unsafe_allow_html=True)
-    text_data = 'DESCRIPTIVE TERMS/ADJECTIVES/ADJECTIVAL PHRASES (e.g. filthy, dirty, undesirable)'  
-    wordcloud = WordCloud(width=600, height=400, background_color='Black', mode='RGBA').generate(' '.join(filtered_df[text_data].dropna()))
-    plt.figure(figsize=(10, 5))
-    plt.imshow(wordcloud, interpolation='bilinear')
-    plt.axis('off')
-    st.set_option('deprecation.showPyplotGlobalUse', False)
-    st.pyplot()
-
-
-    col3, col4 = st.columns((2))
-
-    col3.subheader('Descriptive studies Word Cloud')
-    text_data = 'Descriptive studies( nature of slums(conceptualization), health concerns, housing, water, sanitation, nutrition,land, approaches to slum Upgrading)'  
-    wordcloud = WordCloud(width=400, height=400, background_color='#3498db', mode='RGBA').generate(' '.join(filtered_df[text_data].dropna()))
-    plt.figure(figsize=(10, 5))
-    plt.imshow(wordcloud, interpolation='bilinear')
-    plt.axis('off')
-    st.set_option('deprecation.showPyplotGlobalUse', False)
-    col3.pyplot()
-
-    col3.subheader('Prescriptive Studies Word Cloud')
-    text_data = 'Prescriptive[inclusivity/tackling marginalisation, slum upgrading/improvement, security improvement,'  
-    wordcloud = WordCloud(width=400, height=400, background_color='#2ecc71', mode='RGBA').generate(' '.join(filtered_df[text_data].dropna()))
-    plt.figure(figsize=(10, 5))
-    plt.imshow(wordcloud, interpolation='bilinear')
-    plt.axis('off')
-    st.set_option('deprecation.showPyplotGlobalUse', False)
-    col3.pyplot()
-
-    col4.subheader('Diagnostic Studies Word Cloud')
-    text_data = 'Diagnostic (strategies, policy gaps/challenges, tenure concerns, public health concerns, environmental concerns, marginalisation(inclusion, exclusion)]'  
-    wordcloud = WordCloud(width=400, height=400, background_color='#e74c3c', mode='RGBA').generate(' '.join(filtered_df[text_data].dropna()))
-    plt.figure(figsize=(10, 5))
-    plt.imshow(wordcloud, interpolation='bilinear')
-    plt.axis('off')
-    st.set_option('deprecation.showPyplotGlobalUse', False)
-    col4.pyplot()
-
-    col4.subheader('Theoretical Studies Word Cloud')
-    text_data = 'Theoretical( theories, liberal/neo-liberal theories, colonial/post colonial theories, political economy)'  
-    wordcloud = WordCloud(width=400, height=400, background_color='#f39c12', mode='RGBA').generate(' '.join(filtered_df[text_data].dropna()))
-    plt.figure(figsize=(10, 5))
-    plt.imshow(wordcloud, interpolation='bilinear')
-    plt.axis('off')
-    st.set_option('deprecation.showPyplotGlobalUse', False)
-    col4.pyplot()
-
-else:
-    st.warning('No Data Available')
